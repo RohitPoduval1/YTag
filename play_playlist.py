f5890c11018b23e1452bc6cb1b466f672cb0ad49
Rohit Poduval <rohit.poduval2@gmail.com>
2026-05-27 14:23:55 -0500

    Initial commit

---
+++ play_playlist.py
@@ -0,0 +1,19 @@
+import re
+import sys
+import subprocess
+import csv
+
+
+playlist_csv_path = str(sys.argv[1])
+print(playlist_csv_path)
+tag_of_interest = str(sys.argv[2])
+
+with open(playlist_csv_path) as f:
+    csv_reader = csv.reader(f)
+    ids = [id for (_, id, tags) in csv_reader if tag_of_interest in tags]
+
+urls = [f"https://youtu.be/{id}" for id in ids]
+
+command = "mpv --no-video --script-opts=ytdl_hook-ytdl_path=yt-dlp --ytdl-format=bestaudio".split(" ")
+command.extend(urls)
+subprocess.run(command)
