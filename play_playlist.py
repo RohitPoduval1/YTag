e5529d62d0bb4457ba7e05af66c1af2bc05ac7c9
Rohit Poduval <rohit.poduval2@gmail.com>
2026-05-27 15:06:31 -0500

    if name == main

--- play_playlist.py
+++ play_playlist.py
@@ -1,19 +1,18 @@
-import re
 import sys
 import subprocess
 import csv
 
+if __name__ == "__main__":
+    # python play_playlist.py playlist_csv_path tag_of_interest
+    playlist_csv_path = sys.argv[1]
+    tag_of_interest = sys.argv[2]
 
-playlist_csv_path = str(sys.argv[1])
-print(playlist_csv_path)
-tag_of_interest = str(sys.argv[2])
+    with open(playlist_csv_path) as f:
+        csv_reader = csv.reader(f)
+        ids = [id for (_, id, tags) in csv_reader if tag_of_interest in tags]
 
-with open(playlist_csv_path) as f:
-    csv_reader = csv.reader(f)
-    ids = [id for (_, id, tags) in csv_reader if tag_of_interest in tags]
+    urls = [f"https://youtu.be/{id}" for id in ids]
 
-urls = [f"https://youtu.be/{id}" for id in ids]
-
-command = "mpv --no-video --script-opts=ytdl_hook-ytdl_path=yt-dlp --ytdl-format=bestaudio".split(" ")
-command.extend(urls)
-subprocess.run(command)
+    command = "mpv --no-video --script-opts=ytdl_hook-ytdl_path=yt-dlp --ytdl-format=bestaudio".split(" ")
+    command.extend(urls)
+    subprocess.run(command)
